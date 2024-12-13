3210f32c00c3c3bab16532c1bd55acb28a74ac37
MohamadFawaz01 <mfawaz04.19@gmail.com>
2024-12-14 00:54:35 +0200

    fixedapiimport

--- clothstore-main/clothstore-main/frontend/src/api.js
+++ clothstore-main/clothstore-main/frontend/src/api.js
@@ -1,8 +1,9 @@
 // this file is to handle the connection between the frontend and backend
-// using axios to connect it
+// using axios to connect it which is an api connection library
 import axios from "axios";
 
 const api = axios.create({
   // the port of our fast api application
   baseURL: "http://localhost:8001",
 });
+export default api;
